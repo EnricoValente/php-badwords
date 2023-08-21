ba3344e30e75f9521885246cf7e27d1b34daa3e8
EnricoValente <enricovalente42@gmail.com>
2023-08-21 18:10:10 +0200

    commit

---
+++ index.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+
+        
+
+        <form action="result.php" method="GET">
+            <div>
+                <input type="text" name="badword" value="<?php echo $keyWord;?>">
+            </div>
+            <div>
+                <textarea rows="4" cols="50" name="comment" value="<?php echo $text;?>">
+     
+                </textarea>
+                 
+            </div> 
+            
+            <div>
+                <button type="submit">
+                    Invia
+                </button>
+            </div>
+        </form>
+    </body>
+</html>
+
